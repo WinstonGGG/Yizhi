6d8a3ab9348228f2af536ecd0fb6eed95e764416
Jenny Jiang <Ruoshan.Jiang@github.com>
2020-12-24 17:47:19 +0800

    Create ButtonClick.cs
    button ABC

---
+++ YizhiProject/Assets/Script/ButtonClick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour
+{
+    // called when button is pressed
+    public Button m_A, m_B, m_C;
+
+    void Start()
+    {
+        m_A.onClick.AddListener(delegate { TaskWithParameters("A"); });
+        m_B.onClick.AddListener(delegate { TaskWithParameters("B"); });
+        m_C.onClick.AddListener(delegate { TaskWithParameters("C"); });
+    }
+
+    void TaskWithParameters(string message)
+    {
+        //Output this to console when the Button is clicked
+        Debug.Log(message);
+    }
+}
