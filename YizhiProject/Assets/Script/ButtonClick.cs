e605d9d66ecabf8534d50e6b6da8d57d97e138cb
ScarlettLi <scartt@bu.edu>
2020-12-24 18:32:15 +0800

    buttonClick() added
    print a message clickingt button a, b, c

--- YizhiProject/Assets/Script/ButtonClick.cs
+++ YizhiProject/Assets/Script/ButtonClick.cs
@@ -6,7 +6,7 @@ public class ButtonClick : MonoBehaviour
     // called when button is pressed
     public Button m_A, m_B, m_C;
 
-    void Start()
+    /*void Start()
     {
         m_A.onClick.AddListener(delegate { TaskWithParameters("A"); });
         m_B.onClick.AddListener(delegate { TaskWithParameters("B"); });
@@ -17,5 +17,21 @@ public class ButtonClick : MonoBehaviour
     {
         //Output this to console when the Button is clicked
         Debug.Log(message);
+    } */
+    
+
+    public void TaskOnClickA()
+    {
+        Debug.Log("You have clicked button A!");
+    }
+ 
+    public void TaskOnClickB()
+    {
+        Debug.Log("You have clicked button B!");
+    }
+
+    public void TaskOnClickC()
+    {
+        Debug.Log("You have clicked button C!");
     }
 }
