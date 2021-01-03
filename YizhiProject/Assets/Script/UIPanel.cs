102f86e3a39da4a7ac699ce3c5362b69df36d464
ScarlettLi <scartt@bu.edu>
2021-01-03 15:03:43 +0800

    UIPanel Control
    控制UI的消失和出现

---
+++ YizhiProject/Assets/Script/UIPanel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPanel : MonoBehaviour
+{
+    public RawImage charaAImg;
+    public RawImage charaBImg;
+    public Canvas contentBg;
+    public Text contentTxt;
+
+    public void ShowCharaA(bool value)
+    {
+        charaAImg.enabled = value;
+    }
+    public void ShowCharaB(bool value)
+    {
+        charaBImg.enabled = value;
+    }
+    public void ShowContentBg(bool value)
+    {
+        contentBg.enabled = value;
+    }
+    public void ShowContentText(bool value)
+    {
+        contentTxt.enabled = value;
+    }
+    public void SetContentText(string value)
+    {
+        contentTxt.text = value;
+    }
+}
---
+++ YizhiProject/Assets/Script/UIPanelTestClass.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanelTestClass : MonoBehaviour
+{
+    public UIPanel uiPanel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            uiPanel.ShowCharaA(false);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            uiPanel.ShowCharaB(false);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            uiPanel.ShowContentBg(false);
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            uiPanel.ShowContentText(false);
+        }
+        /* if (Input.GetKeyDown("5"))
+        {
+            uiPanel.ShowContentText("abcde");
+        }
+        */
+    }
+}
